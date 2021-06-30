7b9de3090cd2584743c7027c862bee8c72811526
Soroush <soroush.baghernezhad3@gmail.com>
2021-06-30 10:48:06 +0430

    user creation + channel creation added

--- CLI.py
+++ CLI.py
@@ -1,3 +1,5 @@
+import os
+import datetime
 import psycopg2
 #conn = psycopg2.connect("dbname=dvdrental user=postgres password=password")
 #!/usr/bin/python
@@ -45,14 +47,74 @@ def connect():
         print(db_version)
 
         # close the communication with the PostgreSQL
-        cur.close()
+        #cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+        return cur,conn
+        #if conn is not None:
+            #conn.close()
+            #print('Database connection closed.')
+def exit():
+    n = int(input(" Press 5 to exit : "))
+    if n == 5:
+       os.system("cls") # For Windows
+       #run()
+    else:
+       print(" Invalid Option")
+       exit()
+def displayMainMenu():
+    print("‘ — — — — MENU — — — -’")
+    print("‘ 1. Register User’")
+    print("‘ 2. All Users’")
+    print("‘ 3. All Posts’")
+    print('‘ 4. Get posts by a user’')
+    print('‘ 5. Exit’')
+    print(" — — — — — — — — — —" )
 
+def registerUser(cur,connection):
+    print("------user registeration------")
+    name=input("enter name")
+    username=input("enter username")
+    email=input("enter email address")
+    password=input("enter password")
+    prp=input("enter profile picture storage id")
+    dateofjoin=datetime.date
 
+    # is it correct?
+    #GENERIC ID ?
+    sql = """INSERT INTO user(name,username,email,password,dateofjoin,profilepic)
+                 VALUES(%s,%s,%s,%s,%s,%s) RETURNING id;"""
+    cur.execute(sql,(name,username,email,password,dateofjoin,prp))
+    connection.commit()
+    return cur.fetchone()[0]
+
+def createChannel(cur,connection):
+    print("--------creating channel-----")
+    userid=input("input user id of creator")
+    name=input("input name of the channel")
+    creation_date=datetime.date
+    about=input("input about for channel")
+    prp=input("input picture storage id")
+
+    sql="""INSERT INTO channel(name,creation_date,about,pic,count_member) 
+    values(%s,%s,%s,%s)RETURNING id;"""
+    cur.execute(sql,(name,creation_date,about,prp,0))
+    connection.commit();
+    channel_id=cur.fetchone()[0]
+    cur.execute(""" insert into user_channel(user_id,channel_id) values(%s,%s) """,(userid,channel_id))
+    connection.commit()
+
+    return True
+
+    return
 if __name__ == '__main__':
-    connect()
+    cur,connection=connect()
+    print(" pls insert your query")
+    count = cur.rowcount
+    print(count, "count before")
+    cur.execute(""" insert into actor values(%s,%s,%s,%s)""",(13747,"ali","hashemi","2013-05-26 14:47:57.62"))
+
+    connection.commit()
+    count = cur.rowcount
+    print(count, "Record inserted successfully into mobile table")
